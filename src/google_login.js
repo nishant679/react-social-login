74015033a2f6f70c18bf6074a77ee79a1308ff5f
Nishant Kumar <nkr.nishu@gmail.com>
2020-11-05 17:31:53 +0530

    integrated google login

--- src/google_login.js
+++ src/google_login.js
@@ -1,2 +1,30 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+import {GoogleLogin} from 'react-google-login';
+
+
+const LoginG = () =>{
+    const clientId = "682685397242-sajgu06dml71ckcvin5r68rsi72ahoip.apps.googleusercontent.com";
+
+    const responseSuccess = () =>{
+        alert('Success!! ');
+        console.log("Login successful ");
+    }
+
+    const responseFailure = () =>{
+        alert ("Unable to Login !!");
+        console.log("Login failure !");
+    }
+
+    return (
+        <GoogleLogin
+           clientId = {clientId}
+           onSuccess = {responseSuccess}
+           onFailure = {responseFailure}
+           buttonText = "Login"
+           cookiePolicy = {'single_host_origin'}
+        />
+    );
+
+}
+
+export default LoginG;
--- src/loginHome.js
+++ src/loginHome.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import LoginG from './google_login';
 
 const LoginHome = () =>{
 
     return(
         <div className="flex-box">
            <h2 className="login-title">Login to your Account</h2>
-           <button className="btn btn-google">
+           <button className="btn btn-google"
+           onClick = {LoginG}
+           >
            <i class="fa fa-google"></i>
            continue with google </button>
+           <LoginG className ="btn btn-google"/>
            <button className="btn btn-fb"> continue with facebook  </button>
            <button className="btn btn-linkedIn"> continue with LinkedIn </button>
         </div>
