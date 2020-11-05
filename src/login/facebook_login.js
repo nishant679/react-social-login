dd1b823fba2a0a49852730c7e73b66df165259c6
Nishant Kumar <nkr.nishu@gmail.com>
2020-11-05 19:07:46 +0530

    added facebook login button

---
+++ src/login/facebook_login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+    console.log(response);
+  }
+   
+
+const LoginFB = () =>{
+      const appId  = "1100255976982023";
+
+      const componentClicked = () =>{
+          console.log("facebook login btn clicked");
+      }
+
+      return (
+          <FacebookLogin 
+             appId = {appId}
+             autoLoad = {true}
+             fields = "name, email, picture"
+             onClick = {componentClicked}
+             callback = {responseFacebook}
+          />
+      );
+}
+
+export {LoginFB};
--- src/loginHome.js
+++ src/loginHome.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginG from './google_login';
+import {LoginFB} from './login/facebook_login';
 
 const LoginHome = () =>{
 
     return(
         <div className="flex-box">
            <h2 className="login-title">Login to your Account</h2> 
-           <LoginG className ="btn btn-google"/>
+           <LoginG />
+           <LoginFB />
            <button className="btn btn-fb"> continue with facebook  </button>
            <button className="btn btn-linkedIn"> continue with LinkedIn </button>
         </div>
